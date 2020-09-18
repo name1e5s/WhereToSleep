c663b90700e1e8a04b64b3733db4c124d806c76a
name1e5s <name1e5s@qq.com>
2020-09-19 01:26:52 +0800

    Add github

--- src/frontend/src/MainNavBar.js
+++ src/frontend/src/MainNavBar.js
@@ -5,8 +5,9 @@ const MainNavbar = () => {
     const title = "空闲教室查询"
   
     return (
-          <Navbar type="dark" theme="primary" expand="md" align="center">
-            <NavbarBrand href="#">{title}</NavbarBrand>
+          <Navbar full type="dark" theme="primary" expand="xs" align="center">
+            <NavbarBrand href="#" className="mr-auto">{title}</NavbarBrand>
+            <NavbarBrand href="https://github.com/name1e5s/WhereToSleep" className="ml-auto">GitHub</NavbarBrand>
           </Navbar>
     );
 };
